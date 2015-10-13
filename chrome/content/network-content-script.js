d2efb301f345a4e32ff712b440ddc35ce0138e2f
Jan Odvarko <odvarko@gmail.com>
2015-10-13 13:27:22 +0200

    #12 Render WS icon and handle click event

---
+++ chrome/content/network-content-script.js
@@ -0,0 +1,46 @@
+/* See license.txt for terms of usage */
+
+/**
+ * Handle new requests addition.
+ *
+ * Instead of handling 'REQUEST_ADDED' we need to catch
+ * 'RECEIVED_REQUEST_HEADERS' where request headers are
+ * available.
+ *
+ * In case we are dealing with WebSocket upgrade headers
+ * we append a new little icon to the far left side to
+ * indicate that this HTTP request initiated WebSocket
+ * connection.
+ *
+ * The icons serves also as a link to the 'Web Socket'
+ * panel.
+ */
+window.on(EVENTS.RECEIVED_REQUEST_HEADERS, (event, from) => {
+  var requestItem = NetMonitorView.RequestsMenu.getItemByValue(from);
+  var attachment = requestItem.attachment;
+  var requestHeaders = attachment.requestHeaders;
+
+  // Find the 'upgrade' header.
+  var upgradeHeader = requestHeaders.headers.find(header => {
+    return (header.name == "Upgrade");
+  });
+
+  // Bail out if there is no such header of if its value isn't 'websocket'.
+  if (!upgradeHeader || upgradeHeader.value != "websocket") {
+    return;
+  }
+
+  // Append WebSocket icon in the UI
+  var hbox = requestItem._target;
+  var status = hbox.querySelector(".requests-menu-status");
+  status.classList.add("websocket");
+
+  // Register click handler and emit an event that is handled
+  // in the 'WsmNetworkOverlay' overlay.
+  status.addEventListener("click", event => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    window.emit("websocketmonitor:navigate", from);
+  })
+});
